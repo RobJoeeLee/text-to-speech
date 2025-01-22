85c7fe8ec7851eecf0a9028f6995b0b8dcc56623
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2025-01-22 14:35:28 -0500

    selected all the different IDs from the html to use them in javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+const textInput = document.getElementById("text")
+const speedInput = document.getElementById("speed")
+const playButton = document.getElementById("play-button")
+const pauseButton = document.getElementById("pause-button")
+const stopButton = document.getElementById("stop-button")
